bb01b318e0eef37e36f39d2b8b0f5ef26e6fecef
sajan grover <32103555+sajangrover@users.noreply.github.com>
2018-07-03 17:16:43 +0530

    sum of three numbers

--- sum.py
+++ sum.py
@@ -1,4 +1,5 @@
 a = int(input("enter value of a : "))
 b = int(input("enter value of b : "))
+c = int(input("enter value of c : "))
 
-print("sum of a + b = ",a+b)
+print("sum of a + b + c = ",a+b+c)
